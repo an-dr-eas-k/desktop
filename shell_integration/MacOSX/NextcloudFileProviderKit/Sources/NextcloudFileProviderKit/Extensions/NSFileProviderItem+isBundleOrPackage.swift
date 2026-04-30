0c079118153a5f029d1311a45ddc11d916b13f8b
Iva Horn <iva.horn@nextcloud.com>
2026-04-30 17:05:42 +0200

    fix(file-provider): Bundles and packages exclusion
    - Moved file provider extension source code files into NextcloudFileProviderKit to have a single self-contained and testable module for the whole implementation
    - Introduced support for reporting excluded file provider items to the main app and activity stream
    
    Signed-off-by: Iva Horn <iva.horn@nextcloud.com>

---
+++ shell_integration/MacOSX/NextcloudFileProviderKit/Sources/NextcloudFileProviderKit/Extensions/NSFileProviderItem+isBundleOrPackage.swift
@@ -0,0 +1,33 @@
+import FileProvider
+import UniformTypeIdentifiers
+
+extension NSFileProviderItemProtocol {
+    ///
+    /// Determine whether the file provider item is a package or a bundle.
+    ///
+    var isBundleOrPackage: Bool {
+        if contentType?.conforms(to: .bundle) == true {
+            return true
+        }
+
+        if contentType?.conforms(to: .package) == true {
+            return true
+        }
+
+        let fileExtension = filename.fileExtension
+
+        if fileExtension.isEmpty == false {
+            if let type = UTType(filenameExtension: fileExtension) {
+                if type.conforms(to: .applicationBundle) {
+                    return true
+                }
+
+                if type.conforms(to: .application) {
+                    return true
+                }
+            }
+        }
+
+        return false
+    }
+}
